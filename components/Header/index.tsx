6f88675012c54811c22c600c3601585655a4b1c4
Pranav Deshpande <dpranav007@gmail.com>
2022-01-24 15:33:08 +0530

    husky init, font added

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,25 +1,23 @@
-import { Row, Col, Typography, Button } from "antd";
-const { Title } = Typography;
+import { Row, Col, Button } from 'antd'
 
-import styles from "./Header.module.css";
-
+import styles from './Header.module.css'
 const Header = () => {
-  return (
-    <Row className={styles.headerDiv} justify="space-between" align="middle">
-      <Col flex="none"> Logo</Col>
-      <Col xs={0}>
-        <Button type="text" className={styles.headerButton}>
-          Text Button
-        </Button>
-        <Button type="text" className={styles.headerButton}>
-          Text Button
-        </Button>
-        <Button type="text" className={styles.headerButton}>
-          Text Button
-        </Button>
-      </Col>
-    </Row>
-  );
-};
+	return (
+		<Row className={styles.headerDiv} justify='space-between' align='middle'>
+			<Col flex='none'> Logo</Col>
+			<Col xs={0}>
+				<Button type='text' className={styles.headerButton}>
+					Text Button
+				</Button>
+				<Button type='text' className={styles.headerButton}>
+					Text Button
+				</Button>
+				<Button type='text' className={styles.headerButton}>
+					Text Button
+				</Button>
+			</Col>
+		</Row>
+	)
+}
 
-export default Header;
+export default Header
