83b32782fee1ddc961273fb18f2331907b9b6648
Aravindan <aravindanravi33@gmail.com>
2022-01-28 13:51:43 +0530

    fix: fix for testimonials

--- components/Testimonials/index.tsx
+++ components/Testimonials/index.tsx
@@ -3,35 +3,39 @@
 // import Image from 'next/image'
 import React from 'react'
 import styles from './Testimonials.module.css'
+import Masonry from 'react-responsive-masonry'
 
 function Testimonials() {
 	return (
 		<div className={styles.wrapper}>
-			<img
-				src='/assets/images/ss-1.jpg'
-				alt='SS1'
-				style={{ width: '485px', height: '103px', padding: '0.5rem' }}
-			/>
-			<img
-				src='/assets/images/ss-2.jpg'
-				alt='SS1'
-				style={{ width: '441px', height: '275px', padding: '0.5rem' }}
-			/>
-			<img
-				src='/assets/images/ss-3.jpg'
-				alt='SS1'
-				style={{ width: '480px', height: '192px', padding: '0.5rem' }}
-			/>
-			<img
-				src='/assets/images/ss-4.jpg'
-				alt='SS1'
-				style={{ width: '488px', height: '213px', padding: '0.5rem' }}
-			/>
-			<img
-				src='/assets/images/ss-5.jpg'
-				alt='SS1'
-				style={{ width: '441px', height: '236px', padding: '0.5rem' }}
-			/>
+			<Masonry columnsCount={2}>
+				<img
+					src='/assets/images/ss-1.jpg'
+					alt='SS1'
+					className={styles.testimonial}
+				/>
+				<img
+					src='/assets/images/ss-2.jpg'
+					alt='SS1'
+					className={styles.testimonial}
+				/>
+				<img
+					src='/assets/images/ss-3.jpg'
+					alt='SS1'
+					className={styles.testimonial}
+				/>
+				<img
+					src='/assets/images/ss-5.jpg'
+					alt='SS1'
+					className={styles.testimonial}
+				/>
+				<img
+					src='/assets/images/ss-4.jpg'
+					alt='SS1'
+					className={styles.testimonial}
+					style={{ height: '215px', marginBottom: 0 }}
+				/>
+			</Masonry>
 		</div>
 	)
 }
